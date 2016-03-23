3fffcb6c402131b436944c23f990dc0499b9bbc2
ccocchi <cocchi.c@gmail.com>
2016-03-23 11:55:18 +0100

    Namespace redis in tests

--- test/helper.rb
+++ test/helper.rb
@@ -8,7 +8,7 @@ Sidekiq.logger.level = Logger::ERROR
 
 REDIS_URL = ENV['REDIS_URL'] || 'redis://localhost/15'
 Sidekiq.configure_client do |config|
-  config.redis = { url: REDIS_URL }
+  config.redis = { url: REDIS_URL, namespace: 'paquet' }
 end
 
 class TestWorker

6e86ca6c17dcd2418216ec1b5a8430e3defb6e96
David FABREGUETTE <dfabreguette@gmail.com>
2024-07-23 16:40:40 +0200

    fixed : flusher size

--- lib/sidekiq/paquet/flusher.rb
+++ lib/sidekiq/paquet/flusher.rb
@@ -10,7 +10,7 @@ module Sidekiq
       end
 
       def start
-        Sidekiq.logger.info('Starting paquet flusher')
+        Sidekiq.logger.info('Starting paquet flusher 👋')
         @task.execute
       end
 
